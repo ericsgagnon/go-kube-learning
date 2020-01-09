604caf023e5081f39a90d8e120454a5e37645016
Eric <ericsgagnon@gmail.com>
2020-01-09 02:56:51 -0500

    WIP - Explore kubernetes clientset discovery

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ericsgagnon/go-kube-learning
 
 go 1.13
 
-require k8s.io/client-go v0.17.0
+require (
+	gopkg.in/yaml.v2 v2.2.7
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.17.0
+)
--- main.go
+++ main.go
@@ -8,42 +8,80 @@ import (
 	"path/filepath"
 
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"gopkg.in/yaml.v2"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+// Cluster holds current state of a kubenetes cluster and client
+// type Cluster struct {
+// 	CS *kubernetes.Clientset
+// }
+
 func main() {
-	kubeconfigFilePath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	fmt.Println(kubeconfigFilePath)
-
-	kubeconfigFile, err := ioutil.ReadFile(kubeconfigFilePath)
+	// Create kubernetes client
+	cs, err := Client()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	kubeconfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfigFile)
+	sg, err := cs.Discovery().ServerGroups()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	fmt.Println(sg.Groups)
+	fmt.Println(sg.SwaggerDoc())
+	fmt.Println(sg)
+
+	versionInfo, err := cs.ServerVersion()
 	if err != nil {
 		log.Fatalln(err)
-	}
-
-	versionInfo, err := clientset.ServerVersion()
-	if err != nil {
-		log.Fatalln(err)
-
 	}
 	fmt.Println(versionInfo.String())
 	fmt.Println(versionInfo.Platform)
-	csDiscovery, err := clientset.Discovery().OpenAPISchema()
-	if err != nil {
-		log.Fatalln(err)
 
+	output, err := yaml.Marshal(&sg)
+	fmt.Println(string(output))
+	// csDiscovery, err := cs.Discovery().OpenAPISchema()
+	// if err != nil {
+	// 	log.Fatalln(err)
+
+	// }
+
+	// fmt.Println(csDiscovery.Swagger)
+
+	// ds, err := cs.AppsV1().DaemonSets("default").List(metav1.ListOptions{})
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// fmt.Println(ds)
+
+}
+
+// Client returns a pointer to a kubernetes Clientset
+func Client() (*kubernetes.Clientset, error) {
+
+	// manually use ~/.kube/config for now
+	kubeConfigFilePath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	fmt.Println(kubeConfigFilePath)
+
+	kubeConfigFile, err := ioutil.ReadFile(kubeConfigFilePath)
+	if err != nil {
+		return nil, err
 	}
 
-	fmt.Println(csDiscovery.Swagger)
+	kubeConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeConfigFile)
+	if err != nil {
+		return nil, err
+	}
+
+	clientSet, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+	return clientSet, err
 
 }
